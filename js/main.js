1e81f937c3d8a33d520b6a47dbbb858b28c400c4
Ali Sbane <theirisbugs@gmail.com>
2021-07-02 13:40:55 +0100

    Desktop Finished

--- js/main.js
+++ js/main.js
@@ -19,6 +19,8 @@ let cycleTracker = 10;
 
 let scrollBarWidth = undefined;
 
+let howManyUlShouldBeInAVsibleRow = 6;
+
 fetch("./data/data.json")
   .then((res) => res.json())
   .then((item) => {
@@ -124,38 +126,59 @@ function renderToBrowser(item) {
 
   setupSizes();
   progressBarSetup(0, "setup", sliders);
-  listenToClickEventsAndSliderFunc();
+  sliderFunc();
   hoverBox();
 }
 
-function progressBarSetup(pos, action, sliderWithAction) {
-  if (pos < 10) {
-    pos = parseInt(pos.toString().substring(0, 1));
-  } else if (pos >= 10) {
-    pos = parseInt(pos.toString().substring(1, 2));
+function setupSizes() {
+  getScrollBarWidth();
+
+  let windowWidth = window.innerWidth;
+
+  howManyUlShouldBeInAVsibleRow = 6;
+
+  if (windowWidth < 425) {
+    howManyUlShouldBeInAVsibleRow = 3;
+  } else if (windowWidth < 1024) {
+    howManyUlShouldBeInAVsibleRow = 5;
   }
 
-  if (action === "setup") {
-    for (let z = 0; z < sliderWithAction.length; z++) {
-      let ulTagsOfCurrentSlider = document.querySelectorAll(
-        "." + sliderWithAction[z].classList[1] + " .ul-tags"
-      );
-      for (let x = 0; x < progress.length; x++) {
-        let progressSpan = progress[x];
-        progress[x].style.width =
-          200 / Math.floor(ulTagsOfCurrentSlider.length / 6) + "px";
-      }
-    }
-  } else if (action === "slide") {
-    let ulTagsOfCurrentSlider = sliderWithAction.children[0].children;
-    let progressSpanOfCurrentSlider =
-      sliderWithAction.nextElementSibling.children[0];
-    progressSpanOfCurrentSlider.style.left =
-      (200 / Math.floor(ulTagsOfCurrentSlider.length / 6)) * pos + "px";
+  for (let x = 0; x < allUlTags.length; x++) {
+    const ul = allUlTags[x];
+    ul.style.flex =
+      "0 0 calc((100vw - " +
+      (60 + scrollBarWidth + 60 + 20) +
+      "px) / " +
+      howManyUlShouldBeInAVsibleRow;
+    ul.style.height =
+      "calc((100vw - " +
+      (60 + scrollBarWidth + 60 + 20) +
+      "px) / " +
+      howManyUlShouldBeInAVsibleRow;
+  }
+
+  for (let y = 0; y < arrowRight.length; y++) {
+    const arrow = arrowRight[y];
+    arrow.style.width = "60px";
+    arrow.style.height =
+      "calc((100vw - " +
+      (60 + scrollBarWidth + 60 + 20) +
+      "px) / " +
+      howManyUlShouldBeInAVsibleRow;
+  }
+
+  for (let u = 0; u < arrowLeft.length; u++) {
+    const arrow = arrowLeft[u];
+    arrow.style.width = "60px";
+    arrow.style.height =
+      "calc((100vw - " +
+      (60 + scrollBarWidth + 60 + 20) +
+      "px) / " +
+      howManyUlShouldBeInAVsibleRow;
   }
 }
 
-function listenToClickEventsAndSliderFunc() {
+function sliderFunc() {
   for (let a = 0; a < imgTags.length; a++) {
     imgTags[a].addEventListener("load", (e) => {
       liTag = e.target.parentElement;
@@ -200,12 +223,12 @@ function listenToClickEventsAndSliderFunc() {
         setTimeout(() => {
           parentUlTags.style.transition = "none";
           if (loopTracker === 1) {
-            for (let s = 0; s < 5; s++) {
+            for (let s = 0; s < howManyUlShouldBeInAVsibleRow - 1; s++) {
               parentUlTags.appendChild(parentUlTags.firstChild);
             }
             arrowLeftForActiveSlider.style.display = "block";
           } else {
-            for (let s = 0; s < 6; s++) {
+            for (let s = 0; s < howManyUlShouldBeInAVsibleRow; s++) {
               parentUlTags.appendChild(parentUlTags.firstChild);
             }
           }
@@ -246,7 +269,7 @@ function listenToClickEventsAndSliderFunc() {
 
         if (loopTracker === 0) {
           parentUlTags.style.transition = "none";
-          for (let s = 0; s < 5; s++) {
+          for (let s = 0; s < howManyUlShouldBeInAVsibleRow - 1; s++) {
             parentUlTags.insertBefore(
               parentUlTags.lastChild,
               parentUlTags.firstChild
@@ -258,7 +281,7 @@ function listenToClickEventsAndSliderFunc() {
           arrowLeftForActiveSlider.style.display = "none";
         } else {
           parentUlTags.style.transition = "none";
-          for (let s = 0; s < 6; s++) {
+          for (let s = 0; s < howManyUlShouldBeInAVsibleRow; s++) {
             parentUlTags.insertBefore(
               parentUlTags.lastChild,
               parentUlTags.firstChild
@@ -287,27 +310,30 @@ function getScrollBarWidth() {
   scrollBarWidth = window.innerWidth - document.body.clientWidth;
 }
 
-function setupSizes() {
-  getScrollBarWidth();
-
-  for (let x = 0; x < allUlTags.length; x++) {
-    const ul = allUlTags[x];
-    ul.style.flex =
-      "0 0 calc((100vw - " + (60 + scrollBarWidth + 60 + 20) + "px) / 6)";
-    ul.style.height =
-      "calc((100vw - " + (60 + scrollBarWidth + 60 + 20) + "px) / 6)";
+function progressBarSetup(pos, action, sliderWithAction) {
+  if (pos < 10) {
+    pos = parseInt(pos.toString().substring(0, 1));
+  } else if (pos >= 10) {
+    pos = parseInt(pos.toString().substring(1, 2));
   }
 
-  for (let y = 0; y < arrowRight.length; y++) {
-    const arrow = arrowRight[y];
-    arrow.style.width = "60px";
-    arrow.style.height = "calc((100vw - " + (60 + scrollBarWidth + 60 + 20) + "px) / 6)";
-  }
-
-  for (let u = 0; u < arrowLeft.length; u++) {
-    const arrow = arrowLeft[u];
-    arrow.style.width = "60px";
-    arrow.style.height = "calc((100vw - " + (60 + scrollBarWidth + 60 + 20) + "px) / 6)";
+  if (action === "setup") {
+    for (let z = 0; z < sliderWithAction.length; z++) {
+      let ulTagsOfCurrentSlider = document.querySelectorAll(
+        "." + sliderWithAction[z].classList[1] + " .ul-tags"
+      );
+      for (let x = 0; x < progress.length; x++) {
+        let progressSpan = progress[x];
+        progressSpan.style.width =
+          200 / Math.floor(ulTagsOfCurrentSlider.length / 6) + "px";
+      }
+    }
+  } else if (action === "slide") {
+    let ulTagsOfCurrentSlider = sliderWithAction.children[0].children;
+    let progressSpanOfCurrentSlider =
+      sliderWithAction.nextElementSibling.children[0];
+    progressSpanOfCurrentSlider.style.left =
+      (200 / Math.floor(ulTagsOfCurrentSlider.length / 6)) * pos + "px";
   }
 }
 
@@ -321,38 +347,35 @@ function hoverBox() {
     const ul = allUlTags[x];
 
     ul.addEventListener("mouseenter", () => {
-      let parentCurrentUl = ul.parentElement.parentElement;
+      let parentSlider = ul.parentElement.parentElement;
       imageBox = ul.firstChild.firstChild.getAttribute("src");
       classForHoverBox = "hover-box";
-      
-      if(loopTracker === 0) {
-        let firstUl = parentCurrentUl.firstChild.firstChild;  
-        if(ul === firstUl) {
-          classForHoverBox = "hover-box-first-ul"
-        }
-        else {
-          let ulTagsArrangementNow = parentCurrentUl.firstChild.children;
-          if(ul === ulTagsArrangementNow[5]) {
-            classForHoverBox = "hover-box-last-ul"
+
+      if (loopTracker === 0) {
+        let firstUl = parentSlider.firstChild.firstChild;
+        if (ul === firstUl) {
+          classForHoverBox = "hover-box-first-ul";
+        } else {
+          let ulTagsArrangementNow = parentSlider.firstChild.children;
+          if (ul === ulTagsArrangementNow[5]) {
+            classForHoverBox = "hover-box-last-ul";
           }
         }
-      }
-      else {    
-        let firstUl = parentCurrentUl.firstChild.children[1];  
-        if(ul === firstUl) {
-          classForHoverBox = "hover-box-first-ul"
-        }
-        else {
-          let ulTagsArrangementNow = parentCurrentUl.firstChild.children;
-          if(ul === ulTagsArrangementNow[6]) {
-            classForHoverBox = "hover-box-last-ul"
+      } else {
+        let firstUl = parentSlider.firstChild.children[1];
+        if (ul === firstUl) {
+          classForHoverBox = "hover-box-first-ul";
+        } else {
+          let ulTagsArrangementNow = parentSlider.firstChild.children;
+          if (ul === ulTagsArrangementNow[6]) {
+            classForHoverBox = "hover-box-last-ul";
           }
-        }    
+        }
       }
 
       hoveredBox = createElement(
         "div",
-        "." + parentCurrentUl.classList[1] + " ." + ul.classList[1],
+        "." + parentSlider.classList[1] + " ." + ul.classList[1],
         "",
         {
           class: classForHoverBox,
@@ -363,33 +386,35 @@ function hoverBox() {
       id = setTimeout(() => {
         boxAnim(hoveredBox, "show");
       }, 600);
-
     });
 
     ul.addEventListener("mouseleave", () => {
       boxAnim(hoveredBox, "leave");
       clearTimeout(id);
     });
-    
   }
 }
 
 function boxAnim(box, action) {
   let ulWidth = allUlTags[0].clientWidth;
   let idAnim = undefined;
+  let oneTime = true;
 
   if (action === "show") {
     let boxShowWidth = ulWidth;
     animationShow();
     function animationShow() {
       if (boxShowWidth <= ulWidth * 1.4) {
+        if (oneTime === true) {
+          showContentOfHoverBox(box);
+          oneTime = false;
+        }
         boxShowWidth += 10;
         box.style.width = Math.min(boxShowWidth, ulWidth * 1.4) + "px";
         box.style.height = Math.min(boxShowWidth, ulWidth * 1.4) + "px";
         idAnim = requestAnimationFrame(animationShow);
       } else {
         cancelAnimationFrame(idAnim);
-        showContentOfHoverBox(box);
       }
     }
   } else if (action === "leave") {
@@ -411,69 +436,138 @@ function boxAnim(box, action) {
 }
 
 function showContentOfHoverBox(hoveredBox) {
-  let contentHoverBox = createElement("div", "." + hoveredBox.className, "" , {
-    class: "content-hover-box"
+  let contentHoverBox = createElement("div", "." + hoveredBox.className, "", {
+    class: "content-hover-box",
   });
 
-  createElement("button", "." + hoveredBox.firstChild.className, "Play" , {
-    class: "content-hover-play"
-  });
+  let playSVG = createElement(
+    "div",
+    "." + hoveredBox.firstChild.className,
+    "Play",
+    {
+      class: "content-hover-play",
+    }
+  );
 
-  createElement("button", "." + hoveredBox.firstChild.className, "info" , {
-    class: "content-hover-info"
-  });
+  playSVG.innerHTML = `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 426.667 426.667" style="enable-background:new 0 0 426.667 426.667;" xml:space="preserve">
+	<g>
+		<g>
+			<g>
+				<polygon points="170.667,309.333 298.667,213.333 170.667,117.333 			" />
+				<path d="M213.333,0C95.467,0,0,95.467,0,213.333s95.467,213.333,213.333,213.333S426.667,331.2,426.667,213.333
+				S331.2,0,213.333,0z M213.333,384c-94.08,0-170.667-76.587-170.667-170.667S119.253,42.667,213.333,42.667
+				S384,119.253,384,213.333S307.413,384,213.333,384z" />
+			</g>
+		</g>
+	</g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+</svg>`;
 
-  contentHoverBox.addEventListener("click", (e) => {
+  let infoSVG = createElement(
+    "div",
+    "." + hoveredBox.firstChild.className,
+    "info",
+    {
+      class: "content-hover-info",
+    }
+  );
+
+  infoSVG.innerHTML = `<svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="408px" height="408px" viewBox="0 0 408 408" style="enable-background:new 0 0 408 408;" xml:space="preserve">
+	<g>
+		<g id="more-vert">
+			<path d="M204,102c28.05,0,51-22.95,51-51S232.05,0,204,0s-51,22.95-51,51S175.95,102,204,102z M204,153c-28.05,0-51,22.95-51,51
+			s22.95,51,51,51s51-22.95,51-51S232.05,153,204,153z M204,306c-28.05,0-51,22.95-51,51s22.95,51,51,51s51-22.95,51-51
+			S232.05,306,204,306z" />
+		</g>
+	</g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+	<g></g>
+</svg>`;
+
+  infoSVG.addEventListener("click", (e) => {
     showBigBox(e.target);
   });
-
 }
 
 function showBigBox(clicked) {
-  if(clicked.tagName === "BUTTON") {
-    if(clicked.className.includes("play")) {
+  if (clicked.tagName === "DIV") {
+    if (clicked.className.includes("play")) {
       console.log("play");
-    }
-    else if (clicked.className.includes("info")) {
+    } else if (clicked.className.includes("info")) {
       let ul = clicked.parentElement.parentElement;
       let backgroundImage = ul.style.backgroundImage;
       let bigBoxWrapper = createElement("div", "body", "", {
-        class: "big-box-wrapper"
+        class: "big-box-wrapper",
       });
 
       bigBoxWrapper.style.height = document.body.clientHeight + 120 + "px";
 
-      let bigBox = createElement("div","." + bigBoxWrapper.className, "", {
-        class: "big-box"
+      let bigBox = createElement("div", "." + bigBoxWrapper.className, "", {
+        class: "big-box",
       });
 
-      document.body.style.setProperty("--ul-size", allUlTags[0].clientHeight + "px");
-      document.body.style.setProperty("--big-box-left", ul.getBoundingClientRect().left + 40 + "px");
-      document.body.style.setProperty("--big-box-top", ul.getBoundingClientRect().top + 40 + "px");
+      document.body.style.setProperty(
+        "--ul-size",
+        allUlTags[0].clientHeight + "px"
+      );
+      document.body.style.setProperty(
+        "--big-box-left",
+        ul.getBoundingClientRect().left + 40 + "px"
+      );
+      document.body.style.setProperty(
+        "--big-box-top",
+        ul.getBoundingClientRect().top + 40 + "px"
+      );
 
       ul.remove();
 
-
-      createElement("div","." + bigBox.className, "", {
+      createElement("div", "." + bigBox.className, "", {
         class: "bgi-for-big-box",
-        style: "background-image: linear-gradient(to top,#1A1D29fe 50%, #00000000 52%, #00000000 60%) , "+ backgroundImage + ";"
+        style:
+          "background-image: linear-gradient(to top,#1A1D29fe 50%, #00000000 52%, #00000000 60%) , " +
+          backgroundImage +
+          ";",
       });
 
-
       let closeWrapper = createElement("div", "." + bigBox.className, "", {
-        class: "close-wrapper"
-      })
+        class: "close-wrapper",
+      });
 
       createElement("span", "." + closeWrapper.className, "", {});
       createElement("span", "." + closeWrapper.className, "", {});
-
 
       closeWrapper.addEventListener("click", () => {
         bigBoxWrapper.remove();
       });
-
-
     }
   }
 }
 
+window.addEventListener("resize", setupSizes);
